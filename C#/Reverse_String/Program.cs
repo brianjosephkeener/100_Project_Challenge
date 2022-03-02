713fe8589587f68cfc29ed0524021396fb6ebfb9
Brian Keener <brian120496@gmail.com>
2022-03-02 17:21:25 -0500

    revere string finished C#

--- C#/Reverse_String/Program.cs
+++ C#/Reverse_String/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 
+/*
+
+Category: Text
+
+Reverse a String - Enter a string and the program will reverse it and print it out.
+*/
+
 namespace Reverse_String
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input string to reverse now:\n");
+            string input = Console.ReadLine();
+            char[] chararr = input.ToCharArray();
+            for (int i = chararr.Length-1; i >=0 ; i--)
+            {
+                Console.Write(chararr[i]);
+            }
         }
     }
 }
