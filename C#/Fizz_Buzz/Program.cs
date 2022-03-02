4cb6e364cc0eb45f371e61fdb216297b55e2274b
Brian Keener <brian120496@gmail.com>
2022-03-02 16:50:01 -0500

    Fizz_Buzz C#

---
+++ C#/Fizz_Buzz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fizz_Buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
